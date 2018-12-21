fe1bcc74343b79b9b88bb51bc442e159ff64efec
Halcyon <halcyon@sd-117803.dedibox.fr>
2018-12-21 22:26:11 +0100

    gallery

--- hanse_web.py
+++ hanse_web.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, send_from_directory, render_template, send_file
+from werkzeug.contrib.cache import SimpleCache
+
 app = Flask(__name__, static_url_path='')
+cache = SimpleCache()
 
 base_path = "images/"
 
@@ -32,18 +35,18 @@ def file_list():
     for ansi_file in file_list:
         ansi_file = ansi_file[len(base_path):]
         list_html += '--> <a href="/view/' + ansi_file + '">' + ansi_file + '</a><br/>'
-    list_html += "</div>"
+    list_html += '<a href="/gallery">gallery</a></div>'
     return(render_template("default.html", title="files", content=list_html))
 
 @app.route('/gallery')
 def gallery():
     file_list = list(glob.glob(base_path + '*.ans'))
-    list_html = '<h1>Gallery</h1><div style="text-align: left; font-size: 28px; width: 600px;">'
+    list_html = '<h1>Gallery</h1><div style="text-align: left; font-size: 28px;" class="gallery">'
     for ansi_file in file_list:
         ansi_file = ansi_file[len(base_path):]
-        list_html += '<a href="/image/' + ansi_file + '"><img src="/image/' + ansi_file + '?thumb"></img></a><br/>'
-    list_html += "</div>"
-    return(render_template("default.html", title="files", content=list_html))
+        list_html += '<a href="/view/' + ansi_file + '"><img src="/image/' + ansi_file + '?thumb"></img></a>'
+    list_html += '<a href="/"><-- back</a></div>'
+    return(render_template("default.html", title="gallery", content=list_html))
 
 @app.route('/view/<path:path>')
 def render_ansi(path):
@@ -60,10 +63,13 @@ def render_ansi(path):
        return(render_template("default.html", title="Loading error, sorry."))
     
     html_ansi = ""
+    html_ansi += '<div style="text-align: left; font-size: 28px;"><a href="/"><-- back</a></div>'
     for y in range(height):
         for x in range(width):
             char = ansi_image.get_cell(x, y)
             html_ansi += '<span class="fg' + str(char[1]) + ' bg' + str(char[2]) + '">'
+            if char[0] < 32:
+                char[0] = 32
             html_ansi += chr(char[0])
             html_ansi += '</span>'
         html_ansi += "\n"
@@ -81,17 +87,22 @@ def render_ansi_png(path):
         wide_mode = True    
     if request.args.get('thumb', None) != None:
         thumb = True    
+    
+    cached = cache.get(request.url)
+    if not cached is None:
+        bitmap = cached
+    else:
+        try:
+            ansi_image = AnsiImage(ansi_graphics)
+            ansi_image.clear_image(1000, 1000)
+            ansi_image.load_ans(base_path + path, wide_mode = wide_mode)
+            bitmap = ansi_image.to_bitmap(transparent = transparent)
+        except:
+            return(render_template("default.html", title="Loading error, sorry."))
+        if thumb == True:
+            bitmap.thumbnail((128, 128))
+        cache.set(request.url, bitmap)
         
-    try:
-        ansi_image = AnsiImage(ansi_graphics)
-        ansi_image.clear_image(1000, 1000)
-        ansi_image.load_ans(base_path + path, wide_mode = wide_mode)
-        bitmap = ansi_image.to_bitmap(transparent = transparent)
-    except:
-       return(render_template("default.html", title="Loading error, sorry."))
-   
-    if thumb == True:
-        bitmap.thumbnail((128, 128))
     img_io = BytesIO()
     bitmap.save(img_io, 'PNG')
     img_io.seek(0)
